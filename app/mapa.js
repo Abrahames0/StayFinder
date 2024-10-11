982cf653fb0a80fee084b2f356905bb486927e58
Abrahames <abtaham0212@gmail.com>
2024-10-11 01:31:43 -0600

    mapa en el proyecto

--- app/mapa.js
+++ app/mapa.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import Navegacion from "../components/Navegacion";
 import MapPrincipal from "../components/MapPrincipal";
 
